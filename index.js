f05d263c6c91b1e07c85846a3c87611b69865541
abhik199 <abhishekkirar2004@gmail.com>
2024-08-05 16:55:52 +0530

    remote public

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -30,17 +29,7 @@ try {
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "public/dist")));
-
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/dist", "index.html"));
-});
-
-app.get("/test", (req, res) => {
+app.get("/", (req, res) => {
   console.log("Server is running");
   res.send("Server is running");
 });
