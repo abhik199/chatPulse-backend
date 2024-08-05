2e2b29238529ebe64ec255721506174cc281bd5c
abhik199 <abhishekkirar2004@gmail.com>
2024-08-05 19:56:14 +0530

    add public

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -14,6 +15,10 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public/dist")));
 
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
@@ -29,9 +34,14 @@ try {
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   console.log("Server is running");
   res.send("Server is running");
+});
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/dist", "index.html"));
 });
 
 server.listen(PORT, () => {
